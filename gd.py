1b01712734983aa21c23a2bb4062e26f81b46b88
Rameez <rameezkhan.1@hotmail.co.uk>
2017-07-21 20:53:00 +0100

    Gradient descent test and to see its effect of the cost function, also to see the impact of different learning rate from 0.1 to 0.0001 on the cost function, where anything below 0.1 didnt have enough momentum

---
+++ gd.py
@@ -0,0 +1,7 @@
+def gradient_descent_update(x, gradx, learning_rate):
+    """
+    Performs a gradient descent update.
+    """
+    # Return the new value for x
+    x = x - (learning_rate * gradx)
+    return x
